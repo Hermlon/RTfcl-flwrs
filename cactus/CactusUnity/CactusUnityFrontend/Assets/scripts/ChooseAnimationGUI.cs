24832e5a2aa13a3045b26d9cdb0fc47d0d37ffa4
hermlon <hermlon@t-online.de>
2017-06-05 22:42:06 +0200

    Unity frontend

--- cactus/CactusUnity/CactusUnityFrontend/Assets/scripts/ChooseAnimationGUI.cs
+++ cactus/CactusUnity/CactusUnityFrontend/Assets/scripts/ChooseAnimationGUI.cs
@@ -6,17 +6,19 @@ public class ChooseAnimationGUI : MonoBehaviour {
 	private LEDMatrix ledm;
 
 	// Use this for initialization
-	void Start () {
+	void Start () {/*
 		BMLParser bmlParser = new BMLParser();
 		Animation a1 = bmlParser.loadAnimation("Assets/animations_bml/d1_el.bml");
 		ledm = new LEDMatrix();
 		ledm.Init();
 		ledm.playAnimation(a1);
-		Debug.Log(a1.intToBinaryString (12));
+		Debug.Log(a1.intToBinaryString (12));*/
+		UDPSender udp = new UDPSender ("192.168.0.100", 2390);
+		udp.SendMsg("hallo");
 	}
 
 	// Update is called once per frame
 	void Update () {
-		ledm.Update();
+		//ledm.Update();
 	}
 }
--- cactus/CactusUnity/CactusUnityFrontend/Assets/scripts/UDPSender.cs
+++ cactus/CactusUnity/CactusUnityFrontend/Assets/scripts/UDPSender.cs
@@ -1,13 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
 public class UDPSender {
 
-	private string ip = "192.168.0.101";
-	private int port = 2340;
-	private UdpClient client;
+	private string ip;
+	private int port;
 
-	private string sendUdp() {
-		client = new UdpClient (port);
+	IPEndPoint remoteEndPoint;
+	UdpClient client;
+
+	Thread receiveThread;
+	public string lastReceivedUDPPacket="";
+	public string allReceivedUDPPackets=""; 
+
+	public UDPSender (string i, int p, ChooseAnimationGUI listener) {
+		ip = i;
+		port = p;
+		remoteEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+		client = new UdpClient();
+
+	}
+
+	public void SendMsg(string msg) {
+		byte[] data = Encoding.UTF8.GetBytes(msg);
+		client.Send(data, data.Length, remoteEndPoint);
+		receiveThread = new Thread(
+			new ThreadStart(ReceiveData));
+		receiveThread.IsBackground = true;
+		receiveThread.Start();
+	}
+
+	private void ReceiveData() {
+		while (true)
+		{
+			try
+			{
+				// Bytes empfangen.
+				IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0);
+				byte[] data = client.Receive(ref anyIP);
+
+				// Bytes mit der UTF8-Kodierung in das Textformat kodieren.
+				string text = Encoding.UTF8.GetString(data);
+				
+			}
+			catch (Exception err)
+			{
+				//print(err.ToString());
+			}
+		}
+
 	}
 }
